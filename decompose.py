ea319cac7015d1e21192dcf7aa73f58b6495fec1
LConann <chanconan@gmail.com>
2024-05-08 20:45:54 +0800

    rename the file

---
+++ decompose.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def multiplicative_decompose(data, window_length):
+    # trend (perform exponential moving average)
+    trend = data.rolling(window_length).mean()
+    
+    # sesonal component
+    data_detrended = data / trend
+    seasonal = np.zeros(len(data_detrended))
+    for i in range(window_length):
+        idx = np.arange(i, len(data_detrended), window_length, dtype=int)
+        seasonal[idx] = np.mean(data_detrended[idx])
+        
+    # residual component
+    residual = data / seasonal / trend
+    return trend, seasonal, residual
