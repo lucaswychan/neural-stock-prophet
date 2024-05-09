b7de2eb71ab4c13d9929247997be1a7813a6f8a3
LConann <chanconan@gmail.com>
2024-05-09 12:59:14 +0800

    modify the argument to prophet

--- main.py
+++ main.py
@@ -1,39 +1,55 @@
-from src.prophet import NeuralStockProphet
-from src.args import args_parser
-from src.portfolio import RiskParityPortfolio
-from sklearn.preprocessing import MinMaxScaler
-
 import numpy as np
 import pandas as pd
-import os
+from sklearn.preprocessing import MinMaxScaler
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from src.args import args_parser
+from src.portfolio import RiskParityPortfolio
+from src.prophet import NeuralStockProphet
 
 
 def main():
     args = args_parser()
-    
+
     # Initialize the NeuralStockProphet model
-    scaler = MinMaxScaler(feature_range = (0, 1))
-    prophet = NeuralStockProphet(args.stock_names, scaler, args.train_date, args.test_date, args.time_steps, args.window_length, args.factor, args.epochs, args.batch_size, args.lr, args.arima_order, args.arima_trend)
-    
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    prophet = NeuralStockProphet(
+        args.stock_names,
+        scaler,
+        args.train_date[0],
+        args.train_date[1],
+        args.test_date[0],
+        args.test_date[1],
+        args.time_steps,
+        args.window_length,
+        args.factor,
+        args.epochs,
+        args.batch_size,
+        args.lr,
+        args.arima_order,
+        args.arima_trend,
+    )
+
     forecasts, real_vals = prophet.forecast(verbose=args.verbose)
-    
+
     forecasts = np.array([forecasts[stock] for stock in args.stock_names]).T
     real_vals = np.array([real_vals[stock] for stock in args.stock_names]).T
-    
+
     # Initialize the Portfolio model
     Cmat = None
     cvec = None
     Dmat = None
     dvec = None
     constraints = (Cmat, cvec, Dmat, dvec)
-    portfolio = RiskParityPortfolio(forecasts, args.risk_distribution, constraints, seed=1016)
-    
-    analyze_result_df = pd.concat([portfolio.evaluate(forecasts), portfolio.evaluate(real_vals)], axis=0)
+    portfolio = RiskParityPortfolio(
+        forecasts, args.risk_distribution, constraints, seed=1016
+    )
+
+    analyze_result_df = pd.concat(
+        [portfolio.evaluate(forecasts), portfolio.evaluate(real_vals)], axis=0
+    )
     analyze_result_df.index = ["Predicted Portfolio", "True Portfolio"]
     print(analyze_result_df)
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
