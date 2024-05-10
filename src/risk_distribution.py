84dc452a6f1d64e8f5e34bdb9953e23aa4f3efc3
LConann <chanconan@gmail.com>
2024-05-10 21:29:44 +0800

    correct the bugs of method name

--- src/risk_distribution.py
+++ src/risk_distribution.py
@@ -7,9 +7,9 @@ class RiskDistribution:
     # feel free to add your own weights calculation method
     def calculate_budgets(self, risk_distribution):
         if risk_distribution == "eq":
-            return self.equal_weights()
+            return self.equal_budgets()
         elif risk_distribution == "mv":
-            return self.mean_variance_weights()
+            return self.mean_variance_budgets()
         else:
             raise ValueError("Invalid risk distribution: {}".format(risk_distribution))
 
